4a9d246051ad59fe1a87759d6e8a5a228a1cb05f
Nicholas van Zuiden-Rylander <nick.rylander@outlook.com>
2020-08-21 03:37:11 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -32,8 +32,8 @@ def age_appropriate_birthday(birthday_kids)
  
   birthday_kids.each do |name, age|
     if age < 13
-       puts "Happy Birtday #{name}! You are now #{age} years old!"
-    else
+       puts "Happy Birthday #{name}! You are now #{age} years old!"
+    elsif
      age > 12
       puts "You are too old for this."
    end 
